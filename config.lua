b5e0c5fdc49d85fe636839a00817e2d6896b7b72
Muhaddil <151466679+Muhaddil@users.noreply.github.com>
2025-05-19 20:17:35 +0200

    feat(versioning): add auto version checker and update version to v1.0.1
    Introduce an auto version checker to notify users of updates and bump the version to v1.0.1. The checker fetches the latest version from GitHub and displays update information if available. This enhances maintainability and ensures users are aware of new releases.

--- config.lua
+++ config.lua
@@ -1,7 +1,8 @@
 Config = {}
 
-Config.percentage = math.random(20,30)
-Config.itemname = 'moneywash_card'
-Config.progressType = "bar"
-Config.showNotification = true
-Config.returnCard = true
+Config.percentage = math.random(20,30) -- Percentage of money to wash
+Config.itemname = 'moneywash_card' -- Item name to use for the money wash
+Config.progressType = "bar" -- bar | circle | none
+Config.showNotification = true -- Show notification when money is washed if progresstype is none
+Config.returnCard = true -- Return card after money is washed
+Config.AutoVersionChecker = true   -- Enable if you want to check for updates automatically.
